2993978fa94017df09199adfe37110812e021f8b
Victor HG <victorevox@gmail.com>
2019-06-20 15:57:52 -0500

    Change emulator lauch order in script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 /usr/bin/supervisord &
-$ANDROID_HOME/emulator/emulator -avd test-emulator -memory 1500 -no-boot-anim -wipe-data -noaudio -no-snapshot-load -no-accel -gpu swiftshader_indirect -no-snapstorage -no-snapshot-save &
 gnome-screenshot --interactive &
 cd /home/app
 echo n | npm install
+$ANDROID_HOME/emulator/emulator -avd test-emulator -memory 1500 -no-boot-anim -wipe-data -noaudio -no-snapshot-load -no-accel -gpu swiftshader_indirect -no-snapstorage -no-snapshot-save &
 tns build android
-find platforms -name *.apk | xargs npm run e2e -- --runType test --verbose --reuseDevice --appPath
+find platforms -name *.apk | xargs npm run e2e -- --runType test --verbose --reuseDevice --ignoreDeviceController --appPath
 # /bin/bash
