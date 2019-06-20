90c97cc098530f71c969d9f83944469df3f9f6d1
Victor HG <victorevox@gmail.com>
2019-06-20 14:35:11 -0500

    changed travis sripts for android to run docker image instead

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,9 @@
 
 /usr/bin/supervisord &
 $ANDROID_HOME/emulator/emulator -avd test-emulator -memory 1500 -no-boot-anim -wipe-data -noaudio -no-snapshot-load -no-accel -gpu swiftshader_indirect -no-snapstorage -no-snapshot-save &
+gnome-screenshot --interactive &
 cd /home/app
 echo n | npm install
-tns build 
+tns build android
 find platforms -name *.apk | xargs npm run e2e -- --runType test --verbose --reuseDevice --appPath
 # /bin/bash
